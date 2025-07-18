5c997bdd53c95cdb39738a2e50caa29603ee381a
Saloni Kashyap <124189127+salonikashyap7899@users.noreply.github.com>
2025-07-18 21:32:05 +0530

    RTK-Query

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Home from "./Home.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./index.css";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: 
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/contact",
+        element: <p>Contact Us</p>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />,
+);
